17ffe050f09b0e33bbf7f1f9a98911701b1dd2f2
Paul Sanford <me@paulsanford.net>
2022-10-27 22:19:27 -0600

    Better path prompt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{env, process::Stdio};
+use std::{env, path::PathBuf, process::Stdio};
 
 mod command;
 mod parser;
 
 use anyhow::Result;
 use directories::UserDirs;
+use owo_colors::OwoColorize;
 use parser::parse_pest;
 use rustyline::Editor;
 
@@ -15,9 +16,15 @@ fn path_prompt() -> Result<String> {
     let pwd = env::current_dir()?;
 
     if pwd.starts_with(home) {
-        Ok(format!("~/{}", pwd.strip_prefix(home)?.to_string_lossy()))
+        Ok(format!(
+            "~/{}",
+            pwd.strip_prefix(home)?.to_string_lossy().yellow()
+        ))
     } else {
-        Ok(pwd.to_string_lossy().to_string())
+        Ok(format!(
+            "{}",
+            pwd.to_string_lossy().to_string().bright_blue()
+        ))
     }
 }
 
@@ -29,12 +36,15 @@ fn main() -> Result<()> {
         .history_ignore_dups(true)
         .build();
     let mut rl = Editor::<()>::with_config(config)?;
-    rl.load_history(".history")?;
+    let ud = UserDirs::new().expect("user dirs");
+    let mut history = PathBuf::from(ud.home_dir());
+    history.push(".psh_history");
+    rl.load_history(&history)?;
 
     loop {
         let pwd = path_prompt()?;
         let result = rl.readline(&format!("> {} > ", pwd));
-        rl.save_history(".history")?;
+        rl.save_history(&history)?;
 
         let input_line = match result {
             Err(rustyline::error::ReadlineError::Eof) => break,
